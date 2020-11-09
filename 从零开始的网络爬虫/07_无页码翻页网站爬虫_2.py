80c3c78a9d8fc391257578b0c02c2b645969ba47
YongQuan <2437596031@qq.com>
2020-11-09 12:23:28 +0800

    动态爬取，未完成
    Signed-off-by: YongQuan <2437596031@qq.com>

---
+++ 从零开始的网络爬虫/07_无页码翻页网站爬虫_2.py
@@ -0,0 +1,98 @@
+import sqlite3
+import time
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver.common.keys import Keys
+
+
+def get_html(url):
+    try:
+        option = ChromeOptions()
+        option.add_argument('--headless')
+        browser = webdriver.Chrome(options=option)
+        browser.get(url)
+        time.sleep(5)
+        webdriver.ActionChains(browser).key_down(Keys.END)
+        time.sleep(0.5)
+        webdriver.ActionChains(browser).key_up(Keys.END)
+        time.sleep(2)
+        html = browser.page_source
+        return html
+    finally:
+        browser.quit()
+
+
+def get_data(html):
+    html = BeautifulSoup(html, 'html.parser')
+    data = []
+    for item in html.findAll('div', class_='el-row'):
+        movie = []
+        print(item)
+        chinese_name = item.select('a.router-link-exact-active.router-link-active > h2')
+        # english_name = re.findall(findname, item)[0][1]
+        # type = ''
+        # for i in re.findall(findtype, item):
+        #     type += i + ' '
+        # country = re.findall(findinfo, item)[0][0]
+        # time = re.findall(findinfo, item)[0][1]
+        # published = re.findall(findpublished, item)
+        # if len(published) == 1:
+        #     published = published[0]
+        # else:
+        #     published = None
+        # score = re.findall(findscore, item)[0].strip()
+        movie.append(chinese_name)
+        # movie.append(english_name)
+        # movie.append(type.strip())
+        # movie.append(country)
+        # movie.append(time)
+        # movie.append(published)
+        # movie.append(score)
+        # data.append(movie)
+        print(movie)
+        break
+    return data
+
+
+def save_data(data):
+    sqlite = sqlite3.connect('网络爬虫数据库.db')
+    cursor = sqlite.cursor()
+    sql = '''create table 无页码翻页网站爬虫
+    ('中文名称' text primary key not null,
+    '其他名称' text not null,
+    '类型' text not null,
+    '国家' text not null,
+    '时长' text not null,
+    '上映' date,
+    '评分' text)'''
+    try:
+        cursor.execute(sql)
+        sqlite.commit()
+    except sqlite3.OperationalError:
+        print('数据表已存在')
+    for item in data:
+        for index in range(len(item)):
+            item[index] = '"' + str(item[index]) + '"'
+        sql = '''insert into 无页码翻页网站爬虫
+        values(%s)''' % ', '.join(item)
+        cursor.execute(sql)
+    sqlite.commit()
+    sqlite.close()
+
+
+def main():
+    url = 'https://spa3.scrape.center/'
+    """
+    电影数据网站，无反爬，数据通过 Ajax 加载，无页码翻页，下拉至底部刷新，适合 Ajax 分析和动态页面渲染爬取。
+    本程序使用动态网站渲染方法爬取
+    """
+    start = time.time()
+    html = get_html(url)
+    data = get_data(html)
+    # save_data(data)
+    print('运行时间：{:.6f}'.format(time.time() - start))
+
+
+if __name__ == '__main__':
+    main()
